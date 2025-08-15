65965abfd0b9e7f471c8a9578adbcbb2d28a8825
anu526 <anu526@gmail.com>
2025-08-15 19:59:14 +0530

    Use env variable for MongoDB connection

--- DB/Db.js
+++ DB/Db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose")
 
 const connectDB =()=>{
-mongoose.connect("mongodb+srv://Balaji:@karmika123@userauth.buw0dhc.mongodb.net/?retryWrites=true&w=majority&appName=UserAuth",
-    { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log("MongoDB connection failed:", err));
+    mongoose.connect(process.env.MONGODB_URI)
+        .then(() => console.log("✅ Connected to MongoDB successfully"))
+        .catch(err => console.error("❌ MongoDB connection failed:", err));
 }
 
 module.exports = connectDB
