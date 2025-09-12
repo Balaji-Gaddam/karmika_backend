5a094d810447143cd2e71aa5754f878cf904c18c
anu526 <anu526@gmail.com>
2025-09-12 10:59:48 +0530

    Updated cors

--- server.js
+++ server.js
@@ -25,14 +25,23 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.use(
-  cors({
-    origin: API_URL, // your frontend URL
-    credentials: true,              // allow cookies/auth headers
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://karmika-frontend.vercel.app/"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS: " + origin));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
